f1d1b901542e6637dff4250356ca259c250bacba
TheUknownCreator <233474441+TheUknownCreator@users.noreply.github.com>
2026-07-08 20:41:24 +0200

    [FileExplorerRestarter] restore window position

--- FileExplorerRestarter/FileExplorerRestarter.ps1
+++ FileExplorerRestarter/FileExplorerRestarter.ps1
@@ -4,33 +4,49 @@ Add-Type -AssemblyName UIAutomationTypes
 Add-Type @"
 using System;
 using System.Runtime.InteropServices;
+public static class Native {
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT {
+        public int X;
+        public int Y;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WINDOWPLACEMENT {
+        public int length;
+        public int flags;
+        public int showCmd;
+        public POINT ptMinPosition;
+        public POINT ptMaxPosition;
+        public RECT rcNormalPosition;
+    }
 
-public static class Native
-{
-    [DllImport("user32.dll", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool MoveWindow(
-        IntPtr hWnd,
-        int X,
-        int Y,
-        int nWidth,
-        int nHeight,
-        bool bRepaint
-    );
     [DllImport("user32.dll")]
     public static extern IntPtr GetForegroundWindow();
     [DllImport("user32.dll")]
-    public static extern bool SetForegroundWindow(
-        IntPtr hWnd
-    );
+    public static extern bool SetForegroundWindow(IntPtr hWnd);
+    [DllImport("user32.dll", SetLastError=true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
 }
 "@
+$TIMEOUT_MS = 300000
 
 function OpenExplorer {
-    param([Parameter(Mandatory)][string]$Directory, [int]$TimeoutMs = 5000)
+    param([Parameter(Mandatory)][string]$Directory)
     $sh = New-Object -ComObject Shell.Application
     $oldH = @()
-    $oldCount = $sh.Windows().Count()
     foreach ($w in $sh.Windows()) {
         if ($w.FullName.ToLower().EndsWith("\explorer.exe")) {
             $oldH += [int]$w.HWND
@@ -38,7 +54,7 @@ function OpenExplorer {
     }
     Start-Process explorer.exe "`"$Directory`""
     $sw = [System.Diagnostics.Stopwatch]::StartNew()
-    while ($sw.ElapsedMilliseconds -lt $TimeoutMs) {
+    while ($sw.ElapsedMilliseconds -lt $TIMEOUT_MS) {
         Start-Sleep -Milliseconds 200
         foreach ($w in $sh.Windows()) {
             $hwnd = [int]$w.HWND
@@ -61,15 +77,21 @@ function OpenExplorer {
 
 $shell = New-Object -ComObject Shell.Application
 $explorers = @{}
+$wp = New-Object Native+WINDOWPLACEMENT
+$wp.length = [Runtime.InteropServices.Marshal]::SizeOf([type]"Native+WINDOWPLACEMENT")
 foreach ($window in $shell.Windows()) {
     if ($window.FullName.ToLower().EndsWith("\explorer.exe") -and -not $window.Document.Folder.Self.Path.StartsWith("::")) {
         $hwnd = [int]$window.HWND
         if (-not $explorers.ContainsKey($hwnd)) {
+            if (-not [Native]::GetWindowPlacement($hwnd, [ref]$wp)) {
+                throw "Failed to get window placement."
+            }
             $explorers[$hwnd] = [PSCustomObject]@{
-                Left = $window.Left
-                Top = $window.Top
-                Width = $window.Width
-                Height = $window.Height
+                Left        = $wp.rcNormalPosition.Left
+                Top         = $wp.rcNormalPosition.Top
+                Width       = $wp.rcNormalPosition.Right - $wp.rcNormalPosition.Left
+                Height      = $wp.rcNormalPosition.Bottom - $wp.rcNormalPosition.Top
+                ShowCmd     = $wp.showCmd
                 Directories = [System.Collections.Generic.List[string]]::new()
             }
         }
@@ -88,12 +110,11 @@ Stop-Process -Name explorer -Force -ErrorAction SilentlyContinue
 # $initWnd[0].Quit()
 do {
     Start-Sleep -Milliseconds 200
-} until ((New-Object -ComObject Shell.Application).NameSpace(0) -ne $null)
+} until ($null -ne (New-Object -ComObject Shell.Application).NameSpace(0))
 
 foreach ($explorer in $explorers.Values) {
     $dirs = $explorer.Directories
-    $window = OpenExplorer($dirs[0])
-    # [Native]::MoveWindow($window.HWND, $explorer.Left, $explorer.Top, $explorer.Width, $explorer.Height, $true)
+    $window = OpenExplorer($dirs[0])    #TODO handle timeout
     if ($dirs.Count -gt 1) {
         $uiRoot = [System.Windows.Automation.AutomationElement]::FromHandle([IntPtr]$window.HWND)
         $btnNewTabI = $uiRoot.findFirst([System.Windows.Automation.TreeScope]::Descendants, (New-Object System.Windows.Automation.PropertyCondition([System.Windows.Automation.AutomationElement]::AutomationIdProperty, "AddButton"))).GetCurrentPattern([System.Windows.Automation.InvokePattern]::Pattern)
@@ -105,9 +126,11 @@ foreach ($explorer in $explorers.Values) {
                 Start-Sleep -Milliseconds 200
                 $btnAddrBar.SetFocus()
                 $btnAddrBarV.SetValue($dirs[$i])
-            } while (-not $btnAddrBarV.Current.Value.Equals($dirs[$i]))
+            } while (-not $btnAddrBarV.Current.Value.Equals($dirs[$i])) #TODO limit waiting time
             [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
             Start-Sleep -Milliseconds 200   #TODO wait loading
         }
     }
+    [Native]::MoveWindow([IntPtr]$window.HWND, $explorer.Left, $explorer.Top, $explorer.Width, $explorer.Height, $true) | Out-Null
+    [Native]::ShowWindow([IntPtr]$window.HWND, $explorer.showCmd) | Out-Null
 }
